490c85bf432689b66344f2c06131762f6aed3654
AlvinLimo <kerebeialvin69@gmail.com>
2026-02-09 16:54:24 +0300

    deployment steps and fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
